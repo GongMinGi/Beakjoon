1eece8c5719a781853834f691a07c6389b26e3e1
GongMinGi <76468280+GongMinGi@users.noreply.github.com>
2025-03-20 22:26:07 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x, y;
+
+	scanf("%d %d", &x, &y);
+
+	if (x > 0 && y > 0)
+		printf("1\n");
+	else if (x > 0 && y < 0)
+		printf("4\n");
+	else if (x < 0 && y >0)
+		printf("2\n");
+	else if (x < 0 && y < 0)
+		printf("3\n");
+
+	return 0;
+}
