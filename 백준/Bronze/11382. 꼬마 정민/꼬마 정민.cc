25bf81599fb2fc4b1ab4cb0a554a01c68fb1077a
GongMinGi <76468280+GongMinGi@users.noreply.github.com>
2025-03-21 14:33:13 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+
+int main()
+{
+	long long int a, b, c;
+
+	scanf("%lld %lld %lld", &a, &b, &c);
+
+	printf("%lld\n", a + b + c);
+	return 0;
+}
