cebc2093617c4dc4cb5b2a051151f7873915c8ea
GongMinGi <76468280+GongMinGi@users.noreply.github.com>
2025-04-01 23:41:15 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+char s[1001];
+
+int main()
+{
+
+	int n;
+	scanf("%s", s);
+	scanf("%d", &n);
+
+	printf("%c", s[n-1]);
+	
+}
