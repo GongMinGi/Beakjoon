7c9c4a8a4e654f22dcadfb7ed46bd279765fca28
GongMinGi <76468280+GongMinGi@users.noreply.github.com>
2025-05-30 01:34:50 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <stdio.h>
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	int n, a, b;
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d %d", &a, &b);
+
+		printf("Case #%d: %d + %d = %d\n", i + 1, a, b, a + b);
+
+	}
+
+	return 0;
+}
