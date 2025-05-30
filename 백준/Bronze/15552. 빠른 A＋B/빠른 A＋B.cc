333beb367b7d9c6a6a244c8860bec5db2194bef2
GongMinGi <76468280+GongMinGi@users.noreply.github.com>
2025-05-31 04:18:46 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 192 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n, a, b;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+		cout << a + b << "\n";
+
+	}
+
+	return 0;
+
+}
