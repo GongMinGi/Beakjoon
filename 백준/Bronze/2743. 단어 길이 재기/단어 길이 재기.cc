959c7904eb235ebccd3a0b665e827b2b736599a6
GongMinGi <76468280+GongMinGi@users.noreply.github.com>
2025-06-28 22:34:58 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+	string a;
+
+	cin >> a;
+
+	cout << a.length() << endl;
+}
